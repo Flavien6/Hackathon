f930a9a0dcba13d85b561fb1abdaed7520cc167e
Nearrivers <a.fourcade65@gmail.com>
2020-07-01 21:47:38 +0200

    mise au point vues visu

--- frontend/src/components/Client.vue
+++ frontend/src/components/Client.vue
@@ -3,7 +3,6 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <h4>Bootstrap Snipp for Datatable</h4>
                     <div class="table-responsive">
                         <table id="mytable" class="table table-bordred table-striped">                    
                             <thead>                    
@@ -35,19 +34,6 @@
                                 </tr>
                             </tbody>      
                         </table>
-
-                        <div class="clearfix">
-
-                        </div>
-                        <ul class="pagination pull-right">
-                            <li class="disabled"><a href="#"><span class="glyphicon glyphicon-chevron-left"></span></a></li>
-                            <li class="active"><a href="#">1</a></li>
-                            <li><a href="#">2</a></li>
-                            <li><a href="#">3</a></li>
-                            <li><a href="#">4</a></li>
-                            <li><a href="#">5</a></li>
-                            <li><a href="#"><span class="glyphicon glyphicon-chevron-right"></span></a></li>
-                        </ul>
                     </div>                                
                 </div>
             </div>
