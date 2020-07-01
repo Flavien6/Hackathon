9d81d10c9d690ac19a83a74bea9b4a1ca52fe9aa
Tison Florian <tisonflo@hotmail.fr>
2020-07-01 04:28:36 +0200

    waw

--- frontend/src/components/Nav.vue
+++ frontend/src/components/Nav.vue
@@ -1,24 +1,25 @@
 <template>
     <div id="nav">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <NavLink :url="item.url" :name="item.name" />
+        <!--<nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="sidebar-header">
                 <h3>Menu Principal</h3>
             </div>
-            
-            <!-- on definie la class active en fonction de la valeur de activeCollapse -->
+
+            <script>//on definie la class active en fonction de la valeur de activeCollapse</script>
             <span @click="slt" :class="{active:activeCollapse}">test</span>
-            <!--<ul class="list-unstyled components">
+            <ul class="list-unstyled components">
                 <li class="nav-link">
                     <router-link to="/" active-class="active">Accueil</router-link>
                 </li>
                 <li class="nav-link">
                     <router-link to="/visualiser" active-class="active">Visualisation</router-link>
                 </li>
-                
-            </ul>-->
-            
+
+            </ul>
+
         </nav>
-        <!--<div id="content">
+        <div id="content">
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <div class="container-fluid">
 
@@ -34,25 +35,38 @@
 </template>
 
 <script>
+    import NavLink from './NavLink'
+
     export default {
         name: 'Nav',
+        props: {
+
+        },
         data() {
             return {
-                activeCollapse: true
+                linkItems: [
+                    {
+                        name: "Accueil",
+                        url: "/"
+                    },
+                    {
+                        name: "Visualiser",
+                        url: "/visualisez"
+                    }
+                ]
             }
         },
         methods: {
-            slt() {
-                console.log("ouais ouais oauisu")
-                this.activeCollapse = !this.activeCollapse
-                console.log(this.activeCollapse)
-            }
+
         },
         computed: {
 
         },
         mounted() {
-            this.slt()
+
+        },
+        components: {
+            NavLink
         }
     }
 </script>
@@ -60,18 +74,8 @@
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 #nav {
-  height: 0;
-  margin-bottom: 56px;
-  display: flex;
-  width: 100%;
-  align-items: stretch;
-
-  nav {
-  }
-
   a {
     font-weight: bold;
-    color: #2c3e50;
 
     &:hover, &:focus{
         color: inherit;
@@ -83,15 +87,5 @@
       color: #42b983;
     }
   }
-}
-
-@media (max-width: 768px) {
-    nav {
-        margin-left: -250px;
-
-        &:active{
-            margin-left: 0;
-        }
-    }
 }
 </style>
---
+++ frontend/src/components/NavLink.vue
@@ -0,0 +1,32 @@
+<template>
+    <!--:class="{active:isActive}"-->
+    <router-link :to="url" active-class="active" class="nav-link">{{ name }}</router-link>
+</template>
+
+<script>
+    export default {
+        name: 'NavLink',
+        props: {
+            url: String,
+            name: String
+        },
+        data() {
+            return {
+
+            }
+        },
+        methods: {
+
+        },
+        computed: {
+
+        },
+        mounted() {
+
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
